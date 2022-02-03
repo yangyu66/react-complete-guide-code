d0be33f99fd8bcef4ca0473422ff0bd655b5cae4
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-02 22:27:16 -0800

    communicate up - 1

--- code/04-updating-state-that-depends-on-previous-state/src/components/NewExpense/ExpenseForm.js
+++ code/04-updating-state-that-depends-on-previous-state/src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -51,6 +51,8 @@ const ExpenseForm = () => {
     setEnteredDate("")
     setEnteredAmount("1")
     setEnteredTitle("new")
+
+    props.onAddNewExp(enteredItem)
   };
 
   return (
--- code/04-updating-state-that-depends-on-previous-state/src/components/NewExpense/NewExpense.js
+++ code/04-updating-state-that-depends-on-previous-state/src/components/NewExpense/NewExpense.js
@@ -4,9 +4,19 @@ import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = () => {
+
+  const AddNewExpHandler = (expData) => {
+    console.log("in newExp.js")
+    const newData = {
+      ...expData,
+      id: Math.random().toString(),
+    }
+    console.log(newData)
+  }
+
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onAddNewExp={AddNewExpHandler}/>
     </div>
   );
 };
