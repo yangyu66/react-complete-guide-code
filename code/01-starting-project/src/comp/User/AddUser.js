418bc0ec9299de1aac90a9a06b8aaa9c7243d4f3
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-05 20:39:13 -0800

    fetch form input

--- code/01-starting-project/src/comp/User/AddUser.js
+++ code/01-starting-project/src/comp/User/AddUser.js
@@ -8,19 +8,28 @@ const AddUser = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
 
+  const nameChangeHandler = (event) => {
+    console.log(event.target.value)
+    setName(event.target.value)
+  }
+  const ageChangeHandler = (event) => {
+    console.log(event.target.value)
+    setAge(event.target.value)
+  }
   const onSubmitHandler = (event) => {
     console.log("submit");
     event.preventDefault();
-    console.log(event.target);
+    console.log(name, age);
+
   };
 
   return (
     <Card userClass={classes.input}>
       <form onSubmit={onSubmitHandler}>
       <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={nameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
 
       </form>
