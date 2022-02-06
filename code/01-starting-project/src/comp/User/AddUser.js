86d01e796a57ec4d1b690fa3b3a0dea8ccbac07d
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-05 20:44:02 -0800

    2-way binding form

--- code/01-starting-project/src/comp/User/AddUser.js
+++ code/01-starting-project/src/comp/User/AddUser.js
@@ -1,37 +1,47 @@
 import { useState } from "react";
 import classes from "./AddUser.module.css";
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-
+import Card from "../UI/Card";
+import Button from "../UI/Button";
 
 const AddUser = () => {
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
 
   const nameChangeHandler = (event) => {
-    console.log(event.target.value)
-    setName(event.target.value)
-  }
+    console.log(event.target.value);
+    setName(event.target.value);
+  };
   const ageChangeHandler = (event) => {
-    console.log(event.target.value)
-    setAge(event.target.value)
-  }
+    console.log(event.target.value);
+    setAge(event.target.value);
+  };
   const onSubmitHandler = (event) => {
-    console.log("submit");
+    console.log("in submit");
     event.preventDefault();
+    if (name.trim().length === 0 || age.trim().length === 0) {
+      return
+    }
+    if (age < 1) {
+      return
+    }
     console.log(name, age);
-
+    setName("");
+    setAge("");
   };
 
   return (
     <Card userClass={classes.input}>
       <form onSubmit={onSubmitHandler}>
-      <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={nameChangeHandler} />
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={name}
+          onChange={nameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} />
+        <input id="age" type="number" value={age} onChange={ageChangeHandler} />
         <Button type="submit">Add User</Button>
-
       </form>
     </Card>
   );
