923c29319b35d9491e802cd09a8f404077119f42
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-10 20:45:24 -0800

    add reducer in ctx provider

--- code/08-managing-cart-and-modal-state/src/store/CartProvider.js
+++ code/08-managing-cart-and-modal-state/src/store/CartProvider.js
@@ -1,13 +1,37 @@
-import CartContext from './cart-context'
+import { useReducer } from "react";
+
+import CartContext from "./cart-context";
+
+const defaultCart = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updateditems = state.items.concat(action.item);
+    const total = state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updateditems,
+      totalAmount: total,
+    };
+  }
+
+  return defaultCart;
+};
 
 const CartProvider = (props) => {
-  const addItemHandler = (item) => {};
+  const [cartState, cartDispatch] = useReducer(cartReducer, defaultCart);
+
+  const addItemHandler = (item) => {
+    cartDispatch({ type: "ADD", item: item });
+  };
 
   const removeItemHandler = (id) => {};
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemHandler,
     removeItem: removeItemHandler,
   };
