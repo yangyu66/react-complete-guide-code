c6fab65a678059da5c73fb9fb4279a1f084f2fa5
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-10 21:08:31 -0800

    useRef ttoo extract input from other components======

--- code/08-managing-cart-and-modal-state/src/components/Meals/MealItem/MealItemForm.js
+++ code/08-managing-cart-and-modal-state/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,34 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log(event.target.amount_m1)
+    console.log(amountInputRef.current)
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      return;
+    }
+
+  }
+
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
