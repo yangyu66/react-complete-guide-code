0214ff546bb6ac6a968abe47b3204d8573de7758
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-10 17:00:37 -0800

    add ctx and provider

--- code/08-managing-cart-and-modal-state/src/components/Layout/HeaderCartButton.js
+++ code/08-managing-cart-and-modal-state/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
+
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const ctx = useContext(CartContext);
+
+  const numberOfItem = ctx.items.reduce((cartNumber, item) => {
+    return cartNumber + item.amount;
+  }, 0);
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItem}</span>
     </button>
   );
 };
---
+++ code/08-managing-cart-and-modal-state/src/store/cart-context.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CartContext = React.createContext({
+  isLoggedIn: false,
+});
+
+export default CartContext;
+
+
