35df47adc691de9f6b9aa921bee79ddd21e69df1
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-14 20:39:49 -0800

    sth wrong

--- code/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
+++ code/04-onwards-to-a-more-realistic-example/src/hooks/use-http.js
@@ -1,41 +1,43 @@
-import {useEffect, useState } from 'react';
+import { useState } from "react";
 
-const useHttp = (reqConfig, applyData) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-  
-    const sendReq = async (taskText) => {
-      setIsLoading(true);
-      setError(null);
-      try {
-        const response = await fetch(
-            reqConfig.url, {
-                method: reqConfig.method ? reqConfig.method : 'GET',
-                headers: reqConfig.headers ? reqConfig.headers : {},
-                body: JSON.stringify(reqConfig.body) ? JSON.stringify(reqConfig.body) : null
-            }
-        );
-  
-        if (!response.ok) {
-          throw new Error('Request failed!');
-        }
-  
-        const data = await response.json();
-        applyData(data)
-      } catch (err) {
-        setError(err.message || 'Something went wrong!');
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendReq = async (reqConfig, applyData) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(reqConfig.url, {
+        method: reqConfig.method ? reqConfig.method : "GET",
+        headers: reqConfig.headers ? reqConfig.headers : {},
+        body: JSON.stringify(reqConfig.body)
+          ? JSON.stringify(reqConfig.body)
+          : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
       }
-      setIsLoading(false);
-    };
-  
-    // useEffect(() => {
-    //     sendReq();
-    // }, []);
-    return {
-        isLoading: isLoading, 
-        error: error,
-        sendReq: sendReq
-    }
-}
 
-export default useHttp
+      const data = await response.json();
+      applyData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  // useEffect(() => {
+  //     sendReq();
+  // }, []);
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendReq: sendReq,
+  };
+
+  
+};
+
+export default useHttp;
