d2f85478c3d781357cac9b1ba406125230b5be04
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-20 17:17:43 -0800

    add redux store, reduce  and subscribe

--- code/01-starting-project/src/components/Counter.js
+++ code/01-starting-project/src/components/Counter.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+  /// will set up subsriber to this state in store
+  const counter = useSelector(state => state.counter);
+
+  const toggleCounterHandler = () => {
+    console.log("hit")
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+  <div className={classes.value}>-- COUNTER VALUE -- {counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
