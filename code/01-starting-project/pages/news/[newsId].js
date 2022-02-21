51cd1e3dc14db8c7a257894b5689768427f584fc
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-21 14:20:57 -0800

    dddynamic

---
+++ code/01-starting-project/pages/news/[newsId].js
@@ -0,0 +1,13 @@
+// http://localhost:3000/news/sth
+import { useRouter } from 'next/router';
+
+function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  console.log(newsId)
+    return <h1>The Detail Page</h1>
+  }
+  
+  export default DetailPage;
--- code/01-starting-project/pages/news/sth.js
+++
@@ -1,7 +0,0 @@
-// http://localhost:3000/news/sth
-
-function DetailPage() {
-    return <h1>The Detail Page</h1>
-  }
-  
-  export default DetailPage;
