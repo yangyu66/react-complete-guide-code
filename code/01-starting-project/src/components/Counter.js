299fbc0b24ef882456f867b2b4d579d5adcf786a
Chenyang Yu <cyu0@walmartlabs.com>
2022-02-20 19:15:34 -0800

    add Dispatch

--- code/01-starting-project/src/components/Counter.js
+++ code/01-starting-project/src/components/Counter.js
@@ -1,10 +1,21 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
+
   /// will set up subsriber to this state in store
   const counter = useSelector(state => state.counter);
+
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
 
   const toggleCounterHandler = () => {
     console.log("hit")
@@ -13,7 +24,13 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-  <div className={classes.value}>-- COUNTER VALUE -- {counter}</div>
+      <div className={classes.value}>-- COUNTER VALUE -- {counter}</div>
+
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
+
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
